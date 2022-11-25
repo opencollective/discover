b61fca5ef104f50dc6f0a3071566d1a1b834ef62
Gustav Larsson <gustav.larsson@gmail.com>
2022-11-25 14:00:13 +0100

    Update type

--- components/LocationTag.tsx
+++ components/LocationTag.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const LocationPin = ({ className }) => (
+export const LocationPin = ({ className }: { className?: string }) => (
   <svg width="14" height="16" viewBox="0 0 14 16" fill="none" xmlns="http://www.w3.org/2000/svg" className={className}>
     <path
       fillRule="evenodd"
