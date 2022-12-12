6b3e035775d52e15781ef83b561a22eac5761493
Gustav Larsson <gustav.larsson@gmail.com>
2022-12-12 12:00:16 +0100

    Wrap getDump in try/catch if there is no dump directory

--- lib/getDataDump.ts
+++ lib/getDataDump.ts
@@ -4,14 +4,20 @@ import { join } from 'path';
 const dataDirectory = join(process.cwd(), '_dump');
 
 export function getDump(hostSlug: string) {
-  const files = fs.readdirSync(dataDirectory);
-  const fileName = `${hostSlug}.json`;
-  const file = files.find(file => file === fileName);
-  if (file) {
-    const fullPath = join(dataDirectory, `${hostSlug}.json`);
-    const json = fs.readFileSync(fullPath, 'utf8');
-    const data = JSON.parse(json);
-    return data;
+  try {
+    const files = fs.readdirSync(dataDirectory);
+    const fileName = `${hostSlug}.json`;
+    const file = files.find(file => file === fileName);
+    if (file) {
+      const fullPath = join(dataDirectory, `${hostSlug}.json`);
+      const json = fs.readFileSync(fullPath, 'utf8');
+      const data = JSON.parse(json);
+      return data;
+    }
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.log(err.message);
   }
+
   return null;
 }
