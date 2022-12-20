b6501a95b26ffaf94429a2399dd340e10578fe12
Gustav Larsson <gustav.larsson@gmail.com>
2022-12-20 14:34:59 +0100

    Update modal design

--- components/CollectiveModal.tsx
+++ components/CollectiveModal.tsx
@@ -18,7 +18,23 @@ export const collectiveQuery = gql`
       slug
       createdAt
       description
-      updates(limit: 3) {
+      admins: members(role: [ADMIN], limit: 0) {
+        totalCount
+      }
+      ... on Collective {
+        host {
+          name
+          slug
+        }
+      }
+      ... on Fund {
+        host {
+          name
+          slug
+        }
+      }
+
+      updates(limit: 3, onlyPublishedUpdates: true) {
         totalCount
         nodes {
           title
@@ -52,6 +68,8 @@ export default function CollectiveModal({ isOpen, onClose, collective, locale = 
   if (!collective) {
     return null;
   }
+
+  const statsLabelClasses = 'flex items-center text-xs font-bold uppercase text-gray-700';
   return (
     <React.Fragment>
       <Transition appear show={isOpen} as={Fragment}>
@@ -79,112 +97,127 @@ export default function CollectiveModal({ isOpen, onClose, collective, locale = 
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-lg transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                  <Dialog.Title as="h3" className="flex items-center gap-4 text-xl font-medium text-gray-900">
-                    <Avatar src={collective?.imageUrl} height="40px" width="40px" alt={collective?.name} />
-                    <span>{collective?.name}</span>
-                  </Dialog.Title>
-                  <button
-                    className="absolute right-6 top-5 flex h-12 w-12 items-center justify-center rounded-full border text-gray-600 hover:bg-gray-50"
-                    onClick={onClose}
-                  >
-                    <CloseIcon className="" />
-                  </button>
-                  <AnimateHeight id="description" duration={500} height={height}>
-                    <div className="mt-3">
-                      <p className="text-base text-gray-500">{data?.account?.description}</p>
+                <Dialog.Panel className="w-full max-w-lg transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all">
+                  <div className="space-y-4 p-6 pb-3 lg:p-8 lg:pb-4">
+                    <div className="flex items-center justify-between">
+                      <Dialog.Title as="h3" className="flex items-center gap-4 text-xl font-medium text-gray-900">
+                        <Avatar src={collective?.imageUrl} height="40px" width="40px" alt={collective?.name} />
+                        <span>{collective?.name}</span>
+                      </Dialog.Title>
+                      <button
+                        className="flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full border text-gray-600 hover:bg-gray-50 lg:h-12 lg:w-12"
+                        onClick={onClose}
+                      >
+                        <CloseIcon className="" />
+                      </button>
                     </div>
-                  </AnimateHeight>
-                  {(collective.tags?.length > 0 || collective.location?.label) && (
-                    <div className="mt-4 flex flex-wrap gap-2">
-                      {collective.location && (
-                        <LocationTag
-                          setLocationFilter={filter => setFilter({ location: filter })}
-                          location={collective.location}
-                        />
-                      )}
-                      {collective?.tags?.map(tag => (
-                        <span key={tag} className="rounded-full bg-gray-100 px-2 py-1 text-sm text-gray-700">
-                          {tag}
-                        </span>
-                      ))}
-                    </div>
-                  )}
 
-                  <div className="mt-4 grid grid-cols-4 gap-1 rounded bg-gray-50 p-4 text-sm text-gray-600">
-                    <div className="text-black">Total disbursed</div>
-                    <div>
-                      {formatCurrency(Math.abs(collective.stats?.ALL.spent), currency, {
-                        locale,
-                        precision: 0,
-                      })}
+                    {(collective.tags?.length > 0 || collective.location?.label) && (
+                      <div className="flex flex-wrap gap-2">
+                        {collective.location && (
+                          <LocationTag
+                            setLocationFilter={filter => setFilter({ location: filter })}
+                            location={collective.location}
+                          />
+                        )}
+                        {collective?.tags?.map(tag => (
+                          <span key={tag} className="rounded-full bg-gray-100 px-2 py-1 text-sm text-gray-700">
+                            {tag}
+                          </span>
+                        ))}
+                      </div>
+                    )}
+                    <AnimateHeight id="description" duration={500} height={height}>
+                      <div className="space-y-3">
+                        <p className="text-sm text-gray-500">
+                          Fiscal Host:{' '}
+                          <a
+                            className="text-black hover:text-blue-600"
+                            href={`https://opencollective.com/${data?.account?.host?.slug}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            {data?.account?.host?.name}
+                          </a>
+                        </p>
+                        <p className="text-base text-gray-500">{data?.account?.description}</p>
+                      </div>
+                    </AnimateHeight>
+                    <div className="grid grid-cols-2 gap-2 rounded bg-gray-50 p-4 font-medium text-gray-700">
+                      <div className={statsLabelClasses}>Raised</div>
+                      <div>
+                        {formatCurrency(collective.stats?.ALL.raised, currency, {
+                          locale,
+                          precision: 0,
+                        })}
+                      </div>
+                      <div className={statsLabelClasses}>Contributors</div>{' '}
+                      <div>{collective.stats?.ALL.contributors.toLocaleString(locale)}</div>
+                      <div className={statsLabelClasses}>Disbursed</div>
+                      <div>
+                        {formatCurrency(Math.abs(collective.stats?.ALL.spent), currency, {
+                          locale,
+                          precision: 0,
+                        })}
+                      </div>
+                      <div className={statsLabelClasses}>Collective admins</div>
+                      <div>{data?.account?.admins.totalCount}</div>
+                      <div className={statsLabelClasses}>Created</div>
+                      <div>
+                        {data?.account?.createdAt && (
+                          <FormattedDate dateStyle={'medium'} value={data.account.createdAt} />
+                        )}
+                      </div>
                     </div>
-                    <div className="text-black">Total raised</div>
-                    <div>
-                      {formatCurrency(collective.stats?.ALL.raised, currency, {
-                        locale,
-                        precision: 0,
-                      })}
-                    </div>
-                    <div className="text-black">Contributors</div>{' '}
-                    <div>{collective.stats?.ALL.contributors.toLocaleString(locale)}</div>
-                    <div className="text-black">Contributions</div>{' '}
-                    <div>{collective.stats?.ALL.contributions.toLocaleString(locale)}</div>
-                    <div className="text-black">Created</div>
-                    <div>
-                      {data?.account?.createdAt && (
-                        <FormattedDate dateStyle={'medium'} value={data.account.createdAt} />
-                      )}
-                    </div>
-                  </div>
-                  <AnimateHeight id="updates" duration={500} height={height}>
-                    {data?.account?.updates?.nodes?.length > 0 && (
-                      <React.Fragment>
-                        <h4 className="mt-4 mb-1 text-sm text-gray-500">Latest updates</h4>
-                        <div className="flex flex-col gap-2">
-                          {data.account?.updates?.nodes?.map(update => (
-                            <a
-                              key={update.slug}
-                              href={`https://opencollective.com/${collective.slug}/updates/${update.slug}`}
-                              className="flex items-center gap-3 rounded-md p-2 transition-colors duration-100 hover:bg-gray-50"
-                              target="_blank"
-                              rel="noreferrer"
-                            >
-                              <img
-                                src={update.fromAccount.imageUrl.replace('-staging', '')}
-                                alt={update.fromAccount.name}
-                                className="h-8 w-8 flex-shrink-0 rounded-full object-cover"
-                                width="32px"
-                                height="32px"
-                              />
-                              <div className=" relative overflow-hidden">
-                                <div className="flex items-center justify-between gap-4">
-                                  <h2 className="flex-shrink overflow-hidden text-ellipsis whitespace-nowrap text-sm text-gray-900 group-hover:underline">
-                                    {update.title}
-                                  </h2>
-                                  <p className="flex-shrink-0 text-sm text-gray-500">
-                                    <FormattedDate dateStyle={'medium'} value={update.createdAt} />
+
+                    <AnimateHeight id="updates" duration={500} height={height}>
+                      {data?.account?.updates?.nodes?.length > 0 && (
+                        <div>
+                          <h4 className="mb-1 text-base text-gray-800">Updates</h4>
+                          <div className="flex flex-col gap-2">
+                            {data.account?.updates?.nodes?.map(update => (
+                              <a
+                                key={update.slug}
+                                href={`https://opencollective.com/${collective.slug}/updates/${update.slug}`}
+                                className="flex items-center gap-3 rounded-md p-2 transition-colors duration-100 hover:bg-gray-50"
+                                target="_blank"
+                                rel="noreferrer"
+                              >
+                                <img
+                                  src={update.fromAccount.imageUrl.replace('-staging', '')}
+                                  alt={update.fromAccount.name}
+                                  className="h-8 w-8 flex-shrink-0 rounded-full object-cover"
+                                  width="32px"
+                                  height="32px"
+                                />
+                                <div className=" relative overflow-hidden">
+                                  <div className="flex items-center justify-between gap-4">
+                                    <h2 className="flex-shrink overflow-hidden text-ellipsis whitespace-nowrap text-sm text-gray-900 group-hover:underline">
+                                      {update.title}
+                                    </h2>
+                                    <p className="flex-shrink-0 text-sm text-gray-500">
+                                      <FormattedDate dateStyle={'medium'} value={update.createdAt} />
+                                    </p>
+                                  </div>
+                                  <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm text-gray-500">
+                                    {sanitizeHtml(update.summary, { allowedTags: [], allowedAttributes: {} })}
                                   </p>
                                 </div>
-                                <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm text-gray-500">
-                                  {sanitizeHtml(update.summary, { allowedTags: [], allowedAttributes: {} })}
-                                </p>
-                              </div>
-                            </a>
-                          ))}
+                              </a>
+                            ))}
+                          </div>
                         </div>
-                      </React.Fragment>
-                    )}
-                  </AnimateHeight>
-                  <div className="mt-4">
+                      )}
+                    </AnimateHeight>
+                  </div>
+                  <div className="flex justify-center gap-4 border-t px-8 py-5">
                     <a
-                      href={`https://opencollective.com/${collective?.slug}`}
+                      href={`https://opencollective.com/${collective?.slug}/contribute`}
                       target="_blank"
-                      className="inline-flex w-full justify-center rounded-md border border-transparent bg-gray-100 px-4 py-2 text-base font-medium text-gray-900 hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      className="inline-flex justify-center rounded-full border border-transparent bg-[#1761EB] px-4 py-[10px] text-sm font-medium text-white transition-colors hover:bg-[#1659E1] focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 active:bg-[#1153D6]"
                       rel="noreferrer"
-                      // onClick={onClose}
                     >
-                      Learn more on Open Collective
+                      Contribute
                     </a>
                   </div>
                 </Dialog.Panel>
